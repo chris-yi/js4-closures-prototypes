ca76b59bab4adcf4907be091b1cf16b0b7ba4770
chris-yi <kri5to.yi@gmail.com>
2017-10-19 14:35:21 -0600

    finished closures

--- closures.js
+++ closures.js
@@ -15,12 +15,14 @@ function outer() {
   another variable called 'inner'. */
   
   // Code Here
+  var inner = outer()
   
   //Once you do that, invoke inner.
   
+
   //Code Here
   
-  
+  inner()
   
   
   
@@ -52,6 +54,9 @@ function outer() {
   
     //Code Here
   
+    let callJake = callFriend('Jake')
+
+    callJake('435-555-9248');
   
   
   
@@ -69,13 +74,20 @@ function outer() {
   properly. */
   
   //Code Here
+
+  function makeCounter() {
+    var counter = 0;
+    return function() {
+       return counter += 1;
+    }
+  }
   
   //Uncomment this once you make your function
-  //   var count = makeCounter();
-  //   count(); // 1
-  //   count(); // 2
-  //   count(); // 3
-  //   count(); // 4
+    var count = makeCounter();
+    count(); // 1
+    count(); // 2
+    count(); // 3
+    count(); // 4
   
   
   
@@ -106,16 +118,21 @@ function outer() {
   
   
     return {
-
+      inc: function() {
+        return value += 1
+      },
+      dec: function() {
+        return value -= 1
+      }
     }
   }
   
   
   counter = counterFactory(10);
-  // counter.inc() // 11
-  // counter.inc() // 12
-  // counter.inc() // 13
-  // counter.dec() // 12
+  counter.inc() // 11
+  counter.inc() // 12
+  counter.inc() // 13
+  counter.dec() // 12
   
   
   
@@ -142,10 +159,12 @@ function outer() {
     var welcomeText = 'You\'re doing awesome, keep it up ';
   
     // code message function here.
-  
+    return function message() {
+      return `${welcomeText}${firstname} ${lastname}.`
+    }
   
     //Uncommment this to return the value of your message function
-    //return message;
+    return message;
   
   }
   
@@ -184,6 +203,9 @@ function outer() {
     // outside our lexical scope
     return {
       // Code here.
+      publicMethod: function(){
+          return privateMethod()
+      }
     };
   
   })();
@@ -203,6 +225,12 @@ function outer() {
 
     return {
       // Code here
+      addToSecret: function(num) {
+        return secret += num;
+      },
+      takeAwayFromSecret: function(num) {
+        return secret -= num;
+      }
     }
   }
   
@@ -229,11 +257,12 @@ function outer() {
    */
   
   function timeOutCounter() {
+    var j = []
     for (var i = 0; i <= 5; i++) {
+      j.push(i)
       setTimeout(function() {
-          console.log(i)
+        console.log(j.shift())
       }, i * 1000)
     }
   }
   timeOutCounter();
-  
